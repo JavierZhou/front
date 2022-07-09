d78726917740395f29088764589213eff50c29ac
javier <javierzhou128@gmail.com>
2022-07-09 18:02:06 +0800

    add login

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,98 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input
+              type="password"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input
+              type="text"
+              name="title"
+              required
+              lay-verify="required"
+              placeholder="请输入标题"
+              autocomplete="off"
+              class="layui-input"
+            />
+          </div>
+          <div class="layui-form-mid"  @click="GetCaptcha()">
+            <img v-bind:src="b64s" />
+          </div>
+        </div>
+        <button type="button" class="layui-btn">立即登录</button>
+        <a class="imooc-link" href="http://www.layui.itze.cn">忘记密码？</a>
+      </form>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'app',
+  data () {
+    return {
+      b64s: ''
     }
+  },
+  mounted () {
+    this.GetCaptcha()
+  },
+  methods: {
+    GetCaptcha () {
+      axios.get('http://localhost:8000/getCaptcha').then((res) => {
+        console.log(res)
+        const obj = res.data
+        this.b64s = obj.b64s
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  background: #f2f2f2;
+}
+.layui-container {
+  background: #fff;
+}
+input {
+  width: 190px;
+}
+.imooc-link {
+  margin-left: 10px;
+  &:hover {
+    color: #009688
   }
 }
 </style>
